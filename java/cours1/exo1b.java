f68968c7ef69adf8a0095a6997e08d6a275473be
Stegen <thomasstegen.cur@gmail.com>
2016-01-20 12:22:00 -1000

    Several modification

---
+++ java/cours1/exo1b.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caisseandstock;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author thomas
+ */
+public class exo1b {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("saisir : ");
+        String str = scan.nextLine();
+        System.out.println("saisi :  "+str);
+    }
+    
+}
